91267fa5ee552956bc098afa922a1baa8cafd6a0
grumpyman12601 <101584530+grumpyman12601@users.noreply.github.com>
2024-03-05 14:31:03 -0500

    Delete stop.js
    old files

--- stop.js
+++
@@ -1,5 +0,0 @@
-// Add event listener to the "Stop" button
-document.getElementById('stopButton').addEventListener('click', function() {
-  // Stop the operation of your extension
-  // You may want to perform cleanup tasks or stop certain processes here
-});
