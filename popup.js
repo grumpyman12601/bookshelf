8f02bfe55df526bb3a68239ceec2397c57a1acb0
grumpyman12601 <101584530+grumpyman12601@users.noreply.github.com>
2024-03-06 12:08:27 -0500

    Update popup.js
    Fixed the code

--- popup.js
+++ popup.js
@@ -1,6 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var newtabButton = document.getElementById("newtab");
-    newtabButton.addEventListener("click", function() {
-        chrome.runtime.sendMessage({message: "open_new_window"});
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('openNewWindow').addEventListener('click', function() {
+    // Create a new window
+    chrome.windows.create({
+      type: 'popup',
+      focused: false,
+      top: 1,
+      left: 1,
+      height: 1,
+      width: 1,
+      url: 'test2.html'
+    }, function(window) {
+      // Do something with the created window if needed
+      console.log('New window created with ID: ' + window.id);
     });
+  });
 });
